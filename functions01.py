f07e7ff3b2d8d7900701802ff68d9a083d882806
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-24 21:52:31 +0300

    function_as_argument

--- functions01.py
+++ functions01.py
@@ -1,5 +1,15 @@
-def my_function():
-    print("Hello, world!")
+from typing import Callable
 
-f = my_function
-f()
+def add(a: int, b: int) -> int:
+    return a + b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def apply_operation(a: int, b: int, operation: Callable[[int, int], int]) -> int:
+    return operation(a, b)
+
+result_add = apply_operation(5, 3, add)
+result_multiply = apply_operation(5, 3, multiply)
+
+print(result_add, result_multiply)
