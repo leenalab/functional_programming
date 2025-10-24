0f50d2e3bcf1970f8c8cb63da3b131cc3792aec7
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-24 22:04:16 +0300

    func_in_func_dict_finish

--- funct_i_funct_dict.py
+++ funct_i_funct_dict.py
@@ -3,3 +3,29 @@ from typing import Callable, Dict
 # Визначення функції
 
 def add(a: int, b: int) -> int:
+    return a + b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def power(exponent: int) -> Callable[[int],int]:
+    def inner(base: int) -> int:
+        return base ** exponent
+    return inner
+
+# Використання power для створення функцій square та cube
+square = power(2)
+cube = power(3)
+
+operations: Dict[str, Callable] = {
+    'add': add,
+    'multiply': multiply,
+    'square': square,
+    'cube': cube
+}
+
+result_add = operations['add'](20, 30)  # 30
+result_square = operations['square'](10)  # 25
+
+print(result_add)  
+print(result_square)  
