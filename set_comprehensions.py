44cd9b207cc1cbda6c5e50faef8be637efb0464f
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-26 19:06:27 +0200

    set_comprehensions

--- set_comprehensions.py
+++ set_comprehensions.py
@@ -1,1 +1,8 @@
-{new_item for item in iterable if condition}
+# {new_item for item in iterable if condition}
+
+numbers = [1, 4, 1, 3, 2, 5, 2, 6]
+sq = {i ** 2 for i in numbers}
+print(sq)
+
+odd_squares = {x**2 for x in range(1, 10) if x % 2 != 0}
+print(odd_squares)
