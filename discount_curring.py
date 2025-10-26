dd2e40a1203cef8abebc38d7a80dc6d934125e83
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-26 16:12:09 +0200

    apply_discount+curring

--- discount_curring.py
+++ discount_curring.py
@@ -7,3 +7,24 @@ print(discounted_price)
 
 discounted_price = apply_discount(500, 20)  # Знижка 20% на ціну 500
 print(discounted_price)
+
+
+
+from typing import Callable
+
+def discount(discount_percentage: int) -> Callable[[float], float]:
+    def apply_discount(price: float) -> float:
+        return price * (1 - discount_percentage / 100)
+    return apply_discount
+
+# Каррінг в дії
+ten_percent_discount = discount(30)
+twenty_percent_discount = discount(40)
+
+# Застосування знижок
+discounted_price = ten_percent_discount(500)  # 450.0
+print(discounted_price)
+
+discounted_price = twenty_percent_discount(500)  # 400.0
+print(discounted_price)
+
