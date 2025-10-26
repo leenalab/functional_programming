83bdd83ae6d7c6e653eaf18e57779f0cde0b7c42
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-26 21:15:53 +0200

    filter_and_list_comprehension

--- 02_filter_function.py
+++ 02_filter_function.py
@@ -3,3 +3,28 @@
 even_nums = filter(lambda x: x % 2 == 0, range(1, 11))
 
 print(list(even_nums))
+
+
+def is_positive(x):
+    return x > 0
+
+nums = [-2, -1, 0, 1, 2]
+positive_nums = filter(is_positive, nums)
+
+print(list(positive_nums))
+
+some_str = 'Видавництво А-БА-БА-ГА-ЛА-МА-ГА'
+
+new_str = ''.join(list(filter(lambda x: x.islower(), some_str)))
+print(new_str)
+
+# list comprehensions instead filter
+
+nums = [1, 2, 3, 4, 5, 6]
+even_nums = [x for x in nums if x % 2 == 0]
+print(even_nums)
+
+some_str = 'Видавництво А-БА-БА-ГА-ЛА-МА-ГА'
+
+new_str = ''.join([x for x in some_str if x.islower()])
+print(new_str)
